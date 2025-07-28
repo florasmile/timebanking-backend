142a91eb353140629316fd85d3265d670385afd6
dehuihu <dehuihu@gmail.com>
2025-07-27 22:27:10 -0700

    Adds user login test cases

--- timebanking/accounts/tests.py
+++ timebanking/accounts/tests.py
@@ -2,6 +2,7 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
@@ -41,35 +42,41 @@ class TestUserRegistration(APITestCase):
 
 class TestUserLogin(APITestCase):   
     def setUp(self):
-        ## register a user
-        self.register_url = reverse('register') # get actual URL using url name
+        ## create a user
+        User.objects.create_user(username="testuser", email="test001@example.com", password="password123")
         self.login_url = reverse('login')
-        self.user_data = {
-            "username": "testuser",
-            "email": "test000@example.com",
-            "password": "strongpass123",
-            "password2": "strongpass123",
-        }
-        self.client.post(self.register_url, self.user_data, format="json")
     def test_login_user_with_valid_credentials(self):
-        login_data = {
+        valid_login_credentials= {
             "username": "testuser",
-            "password": "strongpass123",
+            "password": "password123",
         }
-        response = self.client.post(self.login_url, self.user_data, format="json")
-        print(response.data)
+        response = self.client.post(self.login_url, valid_login_credentials, format="json")
+        # print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertIn("Token", response.data)
+        self.assertIn("token", response.data)
     def test_login_user_with_invalid_credentials(self):
-        ...
-
+        invalid_login_credentials= {
+            "username": "testuser",
+            "password": "wrongpass123",
+        }
+        response = self.client.post(self.login_url, invalid_login_credentials, format="json")
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
 class TestUserProfile(APITestCase):
     def setUp(self):
         # Create and authenticate a test user
-        ...
-
+        ## create a user
+        User.objects.create_user(username="testuser", email="test001@example.com", password="password123", bio="a test bio", skills="cooking, tutoring")
+        # Create token and authenticate
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.profile_url = reverse('profile')
     def test_get_profile(self):
-        ...
+        response = self.client.get(self.profile_url, format="json")
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
 
     def test_patch_profile(self):
         ...
