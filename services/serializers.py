35b8ca3a0ab1098e6f673959241e4eabdd66e0e6
dehuihu <dehuihu@gmail.com>
2025-08-06 14:41:48 -0700

    Adds city, zip_code and service_type to GET services response

--- services/serializers.py
+++ services/serializers.py
@@ -5,15 +5,17 @@ class ServiceSerializer(serializers.ModelSerializer):
     owner_email = serializers.EmailField(source='owner.email', read_only=True)
     latitude = serializers.FloatField(source='owner.latitude', read_only=True)
     longitude = serializers.FloatField(source='owner.longitude', read_only=True)
+    city=serializers.CharField(source='owner.city', read_only=True)
+    zip_code=serializers.CharField(source='owner.zip_code', read_only=True)
 
     class Meta:
         model = Service
         fields = [
-            'id', 'name', 'category', 'description', 'tags', 'credit_required',
+            'id', 'name', 'category', 'service_type', 'description', 'tags', 'credit_required',
             'created_at', 'is_available', 'average_rating', 'total_sessions',
-            'remaining_sessions', 'owner', 'owner_email', 'latitude', 'longitude'
+            'remaining_sessions', 'owner', 'owner_email', 'latitude', 'longitude', 'city', 'zip_code'
         ]
-        read_only_fields = ['id', 'created_at', 'owner', 'owner_email', 'average_rating', 'remaining_sessions', 'is_available', 'latitude', 'longitude']
+        read_only_fields = ['id', 'created_at', 'owner', 'owner_email', 'average_rating', 'remaining_sessions', 'is_available', 'latitude', 'longitude', 'city', 'zip_code']
         
     def create(self, validated_data):
         total_sessions = validated_data.get('total_sessions')
